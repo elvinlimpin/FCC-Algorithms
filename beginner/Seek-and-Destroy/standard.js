011e5e00f5c6d8af6ed882cdc38ae42c7124b699
Elvin Limpin <elvinlimpin@gmail.com>
2017-05-23 23:51:53 -0600

    Update standard.js

--- beginner/Seek-and-Destroy/standard.js
+++ beginner/Seek-and-Destroy/standard.js
@@ -3,8 +3,7 @@ function destroyer(arr,x,y,z) {
   /* The filter takes away all the values that are
     !x !y and !z from arr
     Filter takes functions as an argument */
-  arr = arr.filter(function(val){
+  return arr = arr.filter(function(val){
     return val !== x && val !== y && val !== z; 
   });
-  return arr;
 }
