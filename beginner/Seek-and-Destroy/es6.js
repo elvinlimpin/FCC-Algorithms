9a3fc5c5422a874d235389861bc8d632108dcd2a
Elvin Limpin <elvinlimpin@gmail.com>
2017-05-23 23:52:51 -0600

    Update es6.js

--- beginner/Seek-and-Destroy/es6.js
+++ beginner/Seek-and-Destroy/es6.js
@@ -3,10 +3,6 @@ function destroyer(arr,x,y,z) {
   
   /* The filter takes away all the values that are
     !x !y and !z from arr
-    Filter takes functions as an argument 
-    Arrow functions shorten this code */
-  arr = arr.filter(val => val !== x)
-           .filter(val => val !== y)
-           .filter(val => val !== z);
-  return arr;
+    Filter takes functions as an argument */
+  return arr.filter(val => val !== x && val !== y && val !== z);
 }
